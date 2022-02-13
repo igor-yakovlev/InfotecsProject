6b1e605749e4f4a8a98a290e0a32797c819c6b98
igor-yakovlev <47921804+igor-yakovlev@users.noreply.github.com>
2022-02-13 17:57:11 +0300

    Refactoring code and add editing users form

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
 // Получение элементов при помощи DOM API
 const table = document.querySelector("table");
-const btnForm = document.querySelector(".form__button")
-const form = document.querySelector("#formDat")
+const btnForm = document.querySelector(".form__button");
+const form = document.querySelector("form");
+const paginContainer = document.querySelector('.pagin');
 
-let getOrder = true;
+// Вспомогательные переменные
+let users;
+let editableUser;
+
+// Функция для обрезки текста
+const sliceText = (text, size) => {
+  if (text.length > size) return text.slice(0, size) + ' ...'}
 
 // Функция создания элементов таблицы
 function createRows(data) {
   const forDelete = document.querySelector("tbody");
+
   if (forDelete) forDelete.remove();
+
   const tbody = document.createElement("tbody");
   const thead = document.createElement("tr");
   thead.innerHTML = `
@@ -18,26 +27,56 @@ function createRows(data) {
 		<th>Eye color</th>
     `;
   tbody.prepend(thead);
+  
+  // data = data.slice(10, 20);
+
   data.forEach(({id, name, about, eyeColor }) => {
     const elem = document.createElement("tr");
+    elem.dataset.type = id;
     elem.innerHTML = `
-            <td data-type=${id}>${name.firstName}</td>
-            <td data-type=${id}>${name.lastName}</td>
-            <td data-type=${id}>
-            <p class="about">
-                ${about}
-            </p>
+            <td>${name.firstName}</td>
+            <td>${name.lastName}</td>
+            <td class="about">
+                ${sliceText(about, 55)}
             </td>
-            <td data-type=${id}>${eyeColor}</td>
+            <td>${eyeColor}</td>
         `;
     tbody.append(elem);
   });
   table.append(tbody);
 }
 
+const getPagin = (data) => {
+  const tdOnPage = 10;
+  const buttonClicked = data.length / tdOnPage;
+  const startFrom = 0;
+for (let i = 1; i <= buttonClicked; i++) {
+  const paginBtn = document.createElement('button');
+  paginBtn.classList.add('pagin__btn');
+  let text = document.createTextNode(`${i}`);
+  paginBtn.appendChild(text);
+  paginContainer.appendChild(paginBtn);
+
+// const dataPag = allRepos.slice(startFrom , startFrom + tdOnPage)
+console.log(startFrom);
+
+}
+}
+
+
+
+
+
+
+
+
+
+// Переменнная для выбора порядка сортировки
+let getOrder = true;
+
 // Функция сортировки
 const sortData = (title, arr) => {
-   
+
   switch (title) {
     case "firstName":
       return arr.sort((a, b) => { 
@@ -47,7 +86,7 @@ const sortData = (title, arr) => {
           if (a.name.firstName > b.name.firstName) return -1;
         }
        });
-      break;
+      
 
     case "lastName":
       return arr.sort((a, b) => {
@@ -57,7 +96,7 @@ const sortData = (title, arr) => {
           if (a.name.lastName > b.name.lastName) return -1;
         }
       });
-      break;
+      
 
     case "about":
       return arr.sort((a, b) => {
@@ -67,7 +106,7 @@ const sortData = (title, arr) => {
           if (a.about > b.about) return -1;
         }
       });
-      break;
+      
 
     case "Eye color":
       return arr.sort((a, b) => {
@@ -77,7 +116,7 @@ const sortData = (title, arr) => {
           if (a.eyeColor > b.eyeColor) return -1;
         }
       });
-      break;
+      
   }
 };
 
@@ -86,47 +125,100 @@ const sortData = (title, arr) => {
 fetch("http://localhost:3000/users")
   .then((data) => data.json())
   .then((res) => {
+    
     users = res;
     createRows(users);
+    
   });
 
 
+
+
 // Событие клика для сортировки
-table.addEventListener("click", (e) => {
-  const target = e.target;
+table.addEventListener("click", ({target}) => {
   if (target.nodeName !== "TH") return;
   getOrder = !getOrder;
-  console.log(target.innerText);
+
+  
 
   sortData(target.innerText, users);
 
   createRows(users);
 });
 
-table.addEventListener("click", (e) => {
-  const target = e.target;
+// Событие клика редактирования
+table.addEventListener("click", ({target}) => {
+
   if (target.nodeName !== "TD") return;
-  console.log(target.getAttribute('data-type'));
-  fetch("http://localhost:3000/users")
-  .then((data) => data.json())
-  .then((res) => {
-    users = res;
-    const findedUser = users.find(el => el.id === target.getAttribute('data-type'))
-    console.log(findedUser)
-    const inpFirstName = document.querySelector('#firstName');
-    const inpLastName = document.querySelector('#lastName');
-    const inpAbout = document.querySelector('#about');
-    inpFirstName.value = `${findedUser.name.firstName}`;
-    inpLastName.value = `${findedUser.name.lastName}`;
-    inpAbout.value = `${findedUser.about}`;
 
-  });
+  const selectRow = target.parentElement;
+  const allRows = document.querySelectorAll('tr');
+
+  const foundUser = users.find(el => el.id === selectRow.getAttribute('data-type'));
+  
+  editableUser = foundUser;
+
+  const form = document.forms.userForm;
+  
+  const {name: {firstName, lastName}, about, eyeColor} = foundUser;
+    form.firstName.value = firstName;
+    form.lastName.value = lastName;
+    form.about.value = about;
+    form.eyeColor.value = eyeColor;
+  
+  //Выделение строки на которой произошел клик и добавление формы для ее редактирования
+  if (selectRow && selectRow.classList.contains('active')) {
+    selectRow.classList.remove('active');
+    form.classList.add('hide');
+    
+  } else {
+    allRows.forEach(tr => {
+      tr.classList.remove('active');
+    })
+    selectRow.classList.add('active');
+    form.classList.remove('hide');
+    
+  }
+  
+
+});
+
+
+//Событие клика пагинации
+paginContainer.addEventListener('click', ({target}) => {
+  console.log(target.innerHTML);
+  let currentPage = target.innerHTML * 10;
+  console.log(currentPage);
+  
+  users = users.slice(currentPage, currentPage + 10);
+  console.log(users);
+  
+  createRows(users);
+});
+
+
+
+
+// Редактирование выбранной строки
+function editData(form) {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+  editableUser.name.firstName = form.firstName.value;
+  editableUser.name.lastName = form.lastName.value;
+  editableUser.about = form.about.value;
+  editableUser.eyeColor = form.eyeColor.value;
+
+
+  const editableRow = document.querySelector(`[data-type="${editableUser.id}"]`);
+
+  console.log(editableUser);
+  
+  console.log(editableRow);
+  createRows(users);
+  form.reset();
+  form.classList.add('hide');
 })
 
+};
 
-btnForm.addEventListener('click', (e) => {
-  e.preventDefault()
-  const data = new FormData(formDat);
-
-  console.log(data);
-})
+editData(form);
